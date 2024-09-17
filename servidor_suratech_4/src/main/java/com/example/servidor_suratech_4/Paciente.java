841ad27748c036e9177c51b47e7c85187e0d35eb
anita3005 <anitacasta0530@gmail.com>
2024-09-17 15:23:12 -0500

    se crea proyecto suratech_4

---
+++ servidor_suratech_4/src/main/java/com/example/servidor_suratech_4/Paciente.java
@@ -0,0 +1,29 @@
+package com.example.servidor_suratech_4;
+
+import java.time.LocalDate;
+
+public class Paciente {
+
+   private Integer id;
+
+   private String nombre;
+
+   private Integer edad;
+
+   private LocalDate fechaNacimiento;
+
+  private   String documeno;
+
+   private String telefono;
+
+   private String copago;
+
+   private String email;
+
+   private String direccion;
+
+   private String ips;
+
+   private  String medicoFamilia;
+
+}
